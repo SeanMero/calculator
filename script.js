e263b9df6ab3f7801cf6542b31ab80e4e9278433
SeanMero <MeroFromVero@gmail.com>
2024-04-10 20:47:10 +0800

    Create initial files and basic maths functions

---
+++ script.js
@@ -0,0 +1,31 @@
+const add = function(a, b) {
+    return a + b;
+};
+
+const subtract = function(a, b) {
+    return a - b;
+};
+
+const multiply = function(a, b) {
+    return a * b;
+};
+
+const divide = function(a, b) {
+    return a / b;
+};
+
+let num1 = ;
+let num2 = ;
+let operator = ;
+
+const operate = function(num1, num2, operator) {
+    if (operator == "+") {
+        return add(num1, num2)
+    } else if (operator == "-") {
+        return subtract (num1, num2)
+    } else if (operator == "*") {
+        return multiply (num1, num2)
+    } else if (operator == "/") {
+        return divide (num1, num2)
+    }
+};
